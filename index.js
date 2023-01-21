1f9473c1e47f5af042838dee3c5ab2f291e5d219
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-20 16:31:06 -0800

    created basic structure of prompts and .then

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+    .prompt([
+     {
+        type: "input",
+        message: "what is the title of your project?",
+        name: "title"
+     },
+     {
+        type: "input",
+        message: "",
+        name: ""
+     }
+    ])
+    .then(res => {
+        fs.writeFile(`README.md`, JSON.stringify(res), (err) => {
+            if (err){
+                console.log(err)
+            } else {
+                console.log('successfully created README.md')
+            }
+        })
+    })
