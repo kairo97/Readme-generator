d71ed9ce0cedc27f3993391f834907cfba6a926f
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-01-21 11:59:04 -0800

    applied several more prompts, and used when function to make several prompts conditional

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ inquirer
         type: "confirm",
         message: "will this project need a table of contents?",
         name: "table",
-        default: "No"
-     }, {
+        default: "no"
+     },
+     {
         type: "input",
         message: "what are the steps for installation?",
         name: "install",
@@ -35,12 +36,22 @@ inquirer
         when: (answers) => answers.table === "Yes"
      },{
         type: "list",
-        name: "liscense",
+        name: "license",
         message: "what liscense are you using on this project?",
-        choices: [""]
-     }
-     
-    ])
+        choices: ['Apache Liscense 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simpliefied" or "FreeBSD" liscense', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0','None', 'Other']
+     },{
+        type: 'input',
+        name: 'Other Liscense',
+        Message: 'Please type the name of your license',
+        when: (answers) => answers.license === 'Other'
+
+     },{
+        type: 'input',
+        name: 'badge',
+        message: ''
+     },{}
+
+    ])     
     // .then(res => {
     //     fs.writeFile(`README.md`, JSON.stringify(res), (err) => {
     //         if (err){
